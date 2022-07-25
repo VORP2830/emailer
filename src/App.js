20fd836207ac13071d418484e969e41dce06f5d8
Victor Osório <victorporto_@hotmail.com>
2022-07-25 08:26:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+    const editorRef = useRef(null);
+    const [email, setemail] = useState("")
+    const Enviar = () => {
+      axios.post("https://apiemailer.herokuapp.com/", 
+      {email: email,
+      editor: editorRef.current.getContent()})
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  <p class="texto">Emailer</p>
+    <div class="conteiner">
+      <div class='box'>
+        <input type="text" class="email" id="email" placeholder="Email" values={email} onChange={e =>setemail(e.target.value)}/>
+        <Editor
+            onInit={(evt, editor) => editorRef.current = editor}
+            initialValue=""
+            init={{
+            height: 500,
+            width:800,
+            menubar: true,
+            plugins: [
+              'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+              'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+              'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount','code'
+            ],
+            toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
+              'alignleft aligncenter alignright alignjustify | ' +
+              'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+            }}
+        />
+        <button onClick={Enviar}  class="enviar">Enviar email</button>
+      </div>
     </div>
+    </>
   );
 }
-
-export default App;
